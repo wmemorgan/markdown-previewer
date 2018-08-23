1463de1dddc8c47f69f159d05d0d9f80017e4675
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-23 13:19:48 +0800

    Add editor code
    Add preview code
    
    Import default content into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.css'
-import Editor from './Components/Editor'
+import marked from 'marked'
+
+
 
 class App extends Component {
+  state = { content: 'This is the content editor' }
+
+  componentWillMount() {
+    const defaultContent = require("./default.md");
+
+    fetch(defaultContent)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        this.setState({
+          content: text
+        })
+      })
+  }
+
+  updateContent = (e) => {
+    this.setState({ content: e.target.value })
+  }
+
   render() {
+    let { content } = this.state
     return (
-      <div>
-        <Editor />
+      <div className="container">
+        <div className="markdown-editor sub-container">
+          <label htmlFor="editor" className="label">Editor</label>
+          <textarea
+            id="editor"
+            className="textarea"
+            value={this.state.content}
+            onChange={this.updateContent}>
+          </textarea>
+        </div>
+        <div id="preview" className="markdown-previewer sub-container">
+          <label htmlFor="preview" className="label">Previewer</label>
+          <div dangerouslySetInnerHTML={{__html:marked(content)}}>
+
+          </div>
+        </div>
       </div>
     );
   }
